4410a802f49c4a4ccb4ed7a7ed384d97185adc23
Sergey Savelyev <sergey.savelyev2020@gmail.com>
2024-07-10 13:20:56 +0400

    представление списка новостей и конкретной новости
    создал views для двух страниц.
    первая отображает список всех новостей.
    вторая конкретную новость.

--- news_paper/newapp/views.py
+++ news_paper/newapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Post
+
+
+class ListOfAllNews(ListView):
+    model = Post
+    ordering = 'date_creation'
+    template_name = 'list_of_all_news.html'
+    context_object_name = 'all_news'
+
+
+class SpecificNews(DetailView):
+    model = Post
+    template_name = 'specific_news.html'
+    context_object_name = 'specific_news'
