2ebb5c3b224abbfedcfee40bed25ebd900eca75b
Sergey Savelyev <sergey.savelyev2020@gmail.com>
2024-07-19 14:52:30 +0300

    редактирование постов - ссылка создания новости
    добавил название всем ссылкам, сразу добавил ссылки на редактирование и
    удаление. ну и конечно ссылка для создания новости.

--- news_paper/newapp/urls.py
+++ news_paper/newapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import ListOfAllNews, SpecificNews, SearchByNews
+from .views import ListOfAllNews, SpecificNews, SearchByNews, NewsCreate
 
 urlpatterns = [
-    path('', ListOfAllNews.as_view()),
-    path('search/', SearchByNews.as_view()),
-    path('<int:pk>', SpecificNews.as_view()),
+    path('', ListOfAllNews.as_view(), name='список всех статей'),
+    path('search/', SearchByNews.as_view(), name='поиск по всем статьям'),
+    path('<int:pk>', SpecificNews.as_view(), name='вывод отдельной статьи'),
+    path('create/', NewsCreate.as_view(), name='создание новости'),
+    # path('<int:pk>/edit/', name='редактирование новости'),
+    # path('<int:pk>/delete/', name='удаление новости'),
 ]
