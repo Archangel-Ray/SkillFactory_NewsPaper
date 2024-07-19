29b10e78a6f05a5d0263e0cc237723daf110a668
Sergey Savelyev <sergey.savelyev2020@gmail.com>
2024-07-19 10:07:38 +0300

    ссылка на поисковую страницу
    сделал ссылку и посмотрел на то, что получилось.
    ну, что... оно работает.
    только пара проблемок.
    с поиском по названию всё просто, там без сбоев.
    
    а вот авторы у меня отображаются объектами. честно сказать, как сделать
    так что б имена появились - понятия не имею. надо разбиравться. и тут
    у меня вопрос к архитектору: зачем это поле вообще было нужно? нельзя
    было использовать Джанговских юзеров? тем более, что их всё равно
    использовали.
    
    и самое главное - дату: всем объяснять как правильно вводить?  я пока
    вспомнил какой там формать должен быть пять минут перебирал, надоело,
    а потом задание дочитал. там в куда послали учится, вот я и пошёл...
    ждите с рассветом!

--- news_paper/newapp/urls.py
+++ news_paper/newapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ListOfAllNews, SpecificNews
+from .views import ListOfAllNews, SpecificNews, SearchByNews
 
 urlpatterns = [
     path('', ListOfAllNews.as_view()),
+    path('search/', SearchByNews.as_view()),
     path('<int:pk>', SpecificNews.as_view()),
 ]
