5bcb44ec9c6cd46af42fba7577facf381a88add8
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-07-31 15:55:34 +0300

    кнопка "Стать Автором" - представление
    вот функция, которая будет добавлять в группу авторов.
    осталась малость.

--- news_paper/news_paper/views.py
+++ news_paper/news_paper/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -9,3 +12,15 @@ class IndexView(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         context['is_not_authors'] = not self.request.user.groups.filter(name='authors').exists()
         return context
+
+
+@login_required
+def upgrade_me(request):
+    """
+    Добавление в группу "Автор"
+    """
+    user = request.user
+    authors_group = Group.objects.get(name='authors')
+    if not request.user.groups.filter(name='authors').exists():
+        authors_group.user_set.add(user)
+    return redirect('/')
