9b572a6f6668bc1d6dd2cd12aaea691c9b7b0de6
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-02 11:24:42 +0300

    страница постов по категории - ссылка

--- news_paper/newapp/urls.py
+++ news_paper/newapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ListOfAllNews, SpecificNews, SearchByNews, NewsCreate, NewsUpdate, NewsDelete
+from .views import ListOfAllNews, SpecificNews, SearchByNews, NewsCreate, NewsUpdate, NewsDelete, ProductsByCategory
 
 urlpatterns = [
     path('', ListOfAllNews.as_view(), name='список всех постов'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('create/', NewsCreate.as_view(), name='создание новости'),
     path('<int:pk>/edit/', NewsUpdate.as_view(), name='редактирование новости'),
     path('<int:pk>/delete/', NewsDelete.as_view(), name='удаление новости'),
+    path('category/<int:pk>', ProductsByCategory.as_view(), name='товары по категории')
 ]
