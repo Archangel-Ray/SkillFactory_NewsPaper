412d8171a32dd77bc2c68d935380c60bad039cdb
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-06 11:47:10 +0300

    ошибка в имени планировщика
    не... ну... я так не играю...
    за какого, название пакета отличается от имени?

--- news_paper/news_paper/settings.py
+++ news_paper/news_paper/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     # планировщик
-    'django-apscheduler',
+    'django_apscheduler',
 
     # подключение провайдеров
     'allauth.socialaccount.providers.google',
