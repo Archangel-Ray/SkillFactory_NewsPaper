160f03b5317a70cea35228c008992962fa5c9f0e
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-06 12:12:40 +0300

    Задание 6.5.1 тестовый запуск планировщика
    оставил адресс Антона. и самое интересное что адрес реальный.
    думал придут сообщения о том что невозможно доставить,
    но ничего не пришло. так что писма доставлены.
    планировщик успел отправить семь раз.

--- news_paper/newapp/management/commands/runapscheduler.py
+++ news_paper/newapp/management/commands/runapscheduler.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
+from django.core.mail import send_mail
 from django.core.management.base import BaseCommand
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
@@ -12,9 +13,13 @@ from django_apscheduler import util
 logger = logging.getLogger(__name__)
 
 
-def my_job():
-    # Your job processing logic here...
-    pass
+def weekly_newsletter():
+    send_mail(
+        'Тестовая отправка сообщение из планировщика',
+        'Это сообщение пришло потому что я запустил планировщик на срабатывание каждые десять секунд',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=['skavik46111@gmail.com', settings.DEFAULT_FROM_EMAIL],
+    )
 
 
 # The `close_old_connections` decorator ensures that database connections, that have become
@@ -41,13 +46,13 @@ class Command(BaseCommand):
         scheduler.add_jobstore(DjangoJobStore(), "default")
 
         scheduler.add_job(
-            my_job,
+            weekly_newsletter,
             trigger=CronTrigger(second="*/10"),  # Every 10 seconds
-            id="my_job",  # The `id` assigned to each job MUST be unique
+            id="weekly_newsletter",  # Идентификатор, присвоенный каждому заданию, ДОЛЖЕН быть уникальным.
             max_instances=1,
             replace_existing=True,
         )
-        logger.info("Added job 'my_job'.")
+        logger.info("Added job 'weekly_newsletter'.")
 
         scheduler.add_job(
             delete_old_job_executions,
