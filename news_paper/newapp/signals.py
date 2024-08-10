1b40edd04e060614375acb50709a2b91f603c835
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-10 15:03:50 +0300

    в сигналах перенаправил функцию через Селери
    долго думал как отправить пост в задачу.
    не сразу сообразил как это сделать через сигнал.
    в конечном итоге всё оказалось просто.

--- news_paper/newapp/signals.py
+++ news_paper/newapp/signals.py
@@ -2,10 +2,10 @@ from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 
 from .models import PostCategory
-from .tasks import signal_to_create_a_new_post
+from .tasks import message_about_create_a_new_post
 
 
 @receiver(m2m_changed, sender=PostCategory)
 def sending_notification_about_a_new_post(sender, instance, **kwargs):
     if kwargs['action'] == 'post_add':
-        signal_to_create_a_new_post(instance)
+        message_about_create_a_new_post.delay(instance.id)
