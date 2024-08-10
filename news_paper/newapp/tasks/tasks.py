ed523f9721aa02c470b5c1806ffdcfaa235a7bc1
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-10 16:08:29 +0300

    перенёс еженедельную рассылку в файл задач
    задача не изменилась совсем. я попробовал использовать
    timezone.now(), но ничего не получилось и я вернулся всё обратно.
    __________________________________________________________________
    в принципе можно было переименовать файл. или вообще пользоваться
    тем же файлом. зачем переносил, спрашивается. просто название
    смутило. это Алиса Миллер создавала файл с таким именем. может
    оно так и надо. не знаю.

--- news_paper/newapp/tasks/tasks.py
+++ news_paper/newapp/tasks/tasks.py
@@ -1,9 +1,12 @@
+from datetime import timedelta, datetime
+
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.utils import timezone
 
-from newapp.models import Post, PostCategory
+from newapp.models import Post, PostCategory, Category
 
 
 @shared_task
@@ -26,3 +29,29 @@ def message_about_create_a_new_post(post_id):
     )
     msg.attach_alternative(email_message, "text/html")
     msg.send()
+
+
+@shared_task
+def get_a_list_of_new_articles():
+    for topic in Category.objects.all():
+        a_week_earlier = datetime.now() - timedelta(days=7)
+        a_week_earlier = timezone.make_aware(a_week_earlier, timezone=timezone.get_current_timezone())
+        print(f'отправка рассылки на тему {topic}')
+        list_posts = Post.objects.filter(post_category=topic, date_creation__gt=a_week_earlier)
+
+        email_message = render_to_string(
+            'mail/weekly_posts_distribution.html',
+            {
+                'by_category': topic,
+                'posts': list_posts
+            },
+        )
+
+        msg = EmailMultiAlternatives(
+            subject=f'Посты за прошедшую неделю на тему {topic}',
+            body='',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to=[user.email for user in topic.subscribers.all()],
+        )
+        msg.attach_alternative(email_message, "text/html")
+        msg.send()
