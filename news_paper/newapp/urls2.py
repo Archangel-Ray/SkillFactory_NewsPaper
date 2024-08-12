5edda26db85bb7801741f3e1b98f3279a0afe0c8
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-12 12:02:46 +0300

    убрал лишний импорт
    в этом проекте такой беспорядок, что проще написать его заново.

--- news_paper/newapp/urls2.py
+++ news_paper/newapp/urls2.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ListOfAllNews, SpecificNews, SearchByNews, NewsCreate, NewsUpdate, NewsDelete
+from .views import NewsCreate, NewsUpdate, NewsDelete
 
 
 urlpatterns = [
