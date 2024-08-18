8ddbdbb279b36e096f9f270e9e652547d0b04b90
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-18 10:41:13 +0300

    Модуль D14.5 - админ-панель - таблица постов
    сделал отображение таблицы со всеми полями.
    (кроме тематики. не знаю как отображать поле многие-ко-многим)
    фильтрацию по автору, рейтингу и дате создания
    поиск по названию и тексту
    и смену категории выделенного

--- news_paper/newapp/admin.py
+++ news_paper/newapp/admin.py
@@ -2,8 +2,28 @@ from django.contrib import admin
 
 from .models import *
 
+
+def set_category_article(modeladmin, request, queryset):
+    queryset.update(category_type='AR')
+
+
+def set_category_news(modeladmin, request, queryset):
+    queryset.update(category_type='NW')
+
+
+set_category_article.short_description = 'Установить статус "Статья"'
+set_category_news.short_description = 'Установить статус "Новость"'
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'text', 'category_type', 'rating', 'date_creation', ]
+    list_filter = ('author', 'rating', 'date_creation')
+    search_fields = ('title', 'text')
+    actions = [set_category_article, set_category_news]
+
+
 admin.site.register(Author)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
 admin.site.register(Comment)
