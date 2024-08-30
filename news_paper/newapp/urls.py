46c60134a39fe1f0e892efbb7d3a01e69dd3e838
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-30 09:32:41 +0300

    Black отформатировал urls

--- news_paper/newapp/urls.py
+++ news_paper/newapp/urls.py
@@ -1,17 +1,30 @@
 from django.urls import path
 from django.views.decorators.cache import cache_page
 
-from .views import ListOfAllNews, SpecificNews, SearchByNews, NewsCreate, NewsUpdate, NewsDelete, ProductsByCategory, \
-    subscribe, unsubscribe
+from .views import (
+    ListOfAllNews,
+    SpecificNews,
+    SearchByNews,
+    NewsCreate,
+    NewsUpdate,
+    NewsDelete,
+    ProductsByCategory,
+    subscribe,
+    unsubscribe,
+)
 
 urlpatterns = [
-    path('', ListOfAllNews.as_view(), name='список всех постов'),
-    path('search/', SearchByNews.as_view(), name='поиск по всем постам'),
-    path('<int:pk>', SpecificNews.as_view(), name='вывод отдельного поста'),
-    path('create/', NewsCreate.as_view(), name='создание новости'),
-    path('<int:pk>/edit/', NewsUpdate.as_view(), name='редактирование новости'),
-    path('<int:pk>/delete/', NewsDelete.as_view(), name='удаление новости'),
-    path('category/<int:pk>', cache_page(60)(ProductsByCategory.as_view()), name='товары по категории'),
-    path('category/<int:pk>/subscribe', subscribe, name='подписка на категорию'),
-    path('category/<int:pk>/unsubscribe', unsubscribe, name='отписаться от категории'),
+    path("", ListOfAllNews.as_view(), name="список всех постов"),
+    path("search/", SearchByNews.as_view(), name="поиск по всем постам"),
+    path("<int:pk>", SpecificNews.as_view(), name="вывод отдельного поста"),
+    path("create/", NewsCreate.as_view(), name="создание новости"),
+    path("<int:pk>/edit/", NewsUpdate.as_view(), name="редактирование новости"),
+    path("<int:pk>/delete/", NewsDelete.as_view(), name="удаление новости"),
+    path(
+        "category/<int:pk>",
+        cache_page(60)(ProductsByCategory.as_view()),
+        name="товары по категории",
+    ),
+    path("category/<int:pk>/subscribe", subscribe, name="подписка на категорию"),
+    path("category/<int:pk>/unsubscribe", unsubscribe, name="отписаться от категории"),
 ]
