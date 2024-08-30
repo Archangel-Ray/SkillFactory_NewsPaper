f1a8d6026d38a6acd53428ac5837634b3669d862
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-30 09:45:44 +0300

    вред кэширования
    кэш сыграл злую шутку.
    ссылка подписки меняется если подписка оформлена и наоборот.
    а так как страница была закэширована, то она узнавала про то,
    что подписка состоялась когда кэш обновиться.
    это кэширование было для примера. я переставил на другую
    страницу. теперь пусть она страдает. будет тот же фокус: пост
    отредактируют, а он не поменяется.
    да, кэшем надо учиться пользоваться.
    за то есть и плюс: я смог проверить все переводы локализации.

--- news_paper/newapp/urls.py
+++ news_paper/newapp/urls.py
@@ -16,15 +16,15 @@ from .views import (
 urlpatterns = [
     path("", ListOfAllNews.as_view(), name="список всех постов"),
     path("search/", SearchByNews.as_view(), name="поиск по всем постам"),
-    path("<int:pk>", SpecificNews.as_view(), name="вывод отдельного поста"),
+    path(
+        "<int:pk>",
+        cache_page(60)(SpecificNews.as_view()),
+        name="вывод отдельного поста",
+    ),
     path("create/", NewsCreate.as_view(), name="создание новости"),
     path("<int:pk>/edit/", NewsUpdate.as_view(), name="редактирование новости"),
     path("<int:pk>/delete/", NewsDelete.as_view(), name="удаление новости"),
-    path(
-        "category/<int:pk>",
-        cache_page(60)(ProductsByCategory.as_view()),
-        name="товары по категории",
-    ),
+    path("category/<int:pk>", ProductsByCategory.as_view(), name="товары по категории"),
     path("category/<int:pk>/subscribe", subscribe, name="подписка на категорию"),
     path("category/<int:pk>/unsubscribe", unsubscribe, name="отписаться от категории"),
 ]
