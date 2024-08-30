f9cbfecc07a8c3f2f7e26d774faec85dcf03fb43
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-30 12:11:24 +0300

    Black отформатировал filters

--- news_paper/newapp/filters.py
+++ news_paper/newapp/filters.py
@@ -6,20 +6,20 @@ from .models import Post, Author
 
 class ProductFilter(FilterSet):
     category_name_one_field = ModelChoiceFilter(
-        field_name='author',
+        field_name="author",
         queryset=Author.objects.all(),
-        label='Автор статьи или новости',
-        empty_label='все'
+        label="Автор статьи или новости",
+        empty_label="все",
     )
 
     more_than_this_date = DateFilter(
-        widget=forms.DateInput(attrs={'type': 'date'}),
-        lookup_expr='date__gte',
-        field_name='date_creation'
+        widget=forms.DateInput(attrs={"type": "date"}),
+        lookup_expr="date__gte",
+        field_name="date_creation",
     )
 
     class Meta:
         model = Post
         fields = {
-            'title': ['icontains'],
+            "title": ["icontains"],
         }
