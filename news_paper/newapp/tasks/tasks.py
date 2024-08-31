5b16c26465d00a0d1804db7aeb14b37c7ca9ac84
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-08-31 13:00:33 +0300

    Модуль D17.3 - локализовал все письма
    тема письма почему-то не переводятся. вообще, часть переводиться,
    а часть нет. не понимаю, где не правильно делаю.
    __________________________________________________________________
    всё разбросано в разных местах.
    возникает вопрос к Архитектору: так и было задумано?
    или это демонстрация того как делать не надо?
    задачи лежат бесполезные, зачем они теперь?

--- news_paper/newapp/tasks/tasks.py
+++ news_paper/newapp/tasks/tasks.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils import timezone
+from django.utils.translation import gettext
 
 from newapp.models import Post, PostCategory, Category
 
@@ -12,18 +13,20 @@ from newapp.models import Post, PostCategory, Category
 @shared_task
 def message_about_create_a_new_post(post_id):
     new_post = Post.objects.get(id=post_id)
-    new_post_category = PostCategory.objects.get(post_through=new_post.id).category_through
+    new_post_category = PostCategory.objects.get(
+        post_through=new_post.id
+    ).category_through
     email_message = render_to_string(
-        'mail/message_about_new_post.html',
+        "mail/message_about_new_post.html",
         {
-            'by_category': new_post_category,
-            'post': new_post,
+            "by_category": new_post_category,
+            "post": new_post,
         },
     )
 
     msg = EmailMultiAlternatives(
         subject=new_post.title,
-        body='',
+        body="",
         from_email=settings.DEFAULT_FROM_EMAIL,
         to=[user.email for user in new_post_category.subscribers.all()],
     )
@@ -35,21 +38,21 @@ def message_about_create_a_new_post(post_id):
 def get_a_list_of_new_articles():
     for topic in Category.objects.all():
         a_week_earlier = datetime.now() - timedelta(days=7)
-        a_week_earlier = timezone.make_aware(a_week_earlier, timezone=timezone.get_current_timezone())
-        print(f'отправка рассылки на тему {topic}')
-        list_posts = Post.objects.filter(post_category=topic, date_creation__gt=a_week_earlier)
+        a_week_earlier = timezone.make_aware(
+            a_week_earlier, timezone=timezone.get_current_timezone()
+        )
+        list_posts = Post.objects.filter(
+            post_category=topic, date_creation__gt=a_week_earlier
+        )
 
         email_message = render_to_string(
-            'mail/weekly_posts_distribution.html',
-            {
-                'by_category': topic,
-                'posts': list_posts
-            },
+            "mail/weekly_posts_distribution.html",
+            {"by_category": topic, "posts": list_posts},
         )
 
         msg = EmailMultiAlternatives(
-            subject=f'Посты за прошедшую неделю на тему {topic}',
-            body='',
+            subject=f"{gettext('Посты за прошедшую неделю на тему')} {topic}",
+            body="",
             from_email=settings.DEFAULT_FROM_EMAIL,
             to=[user.email for user in topic.subscribers.all()],
         )
