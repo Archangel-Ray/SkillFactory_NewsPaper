20704eb361e2a075ddd1221776739fb8fee55d03
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-13 16:49:49 +0300

    Модуль D18.6 - добавил пути для редактирования и удаления

--- news_paper/newapp/urls1.py
+++ news_paper/newapp/urls1.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import NewsCreate, NewsUpdate, NewsDelete, ListOfNews
+from .views import NewsCreate, NewsUpdate, NewsDelete, ListOfNews, PostAPIUpdate
 
 urlpatterns = [
     path("", ListOfNews.as_view(), name="список всех новости"),
+    path("<int:pk>", PostAPIUpdate.as_view(), name="api редактирование новости"),
     path("create/", NewsCreate.as_view(), name="создание новости"),
     path("<int:pk>/edit/", NewsUpdate.as_view(), name="редактирование новости"),
     path("<int:pk>/delete/", NewsDelete.as_view(), name="удаление новости"),
--- news_paper/newapp/urls2.py
+++ news_paper/newapp/urls2.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import NewsCreate, NewsUpdate, NewsDelete, ListOfArticles
+from .views import NewsCreate, NewsUpdate, NewsDelete, ListOfArticles, PostAPIUpdate
 
 urlpatterns = [
     path("", ListOfArticles.as_view(), name="список всех статей"),
+    path("<int:pk>", PostAPIUpdate.as_view(), name="api редактирование статьи"),
     path('create/', NewsCreate.as_view(), name='создание статьи'),
     path('<int:pk>/edit/', NewsUpdate.as_view(), name='редактирование статьи'),
     path('<int:pk>/delete/', NewsDelete.as_view(), name='удаление статьи'),
