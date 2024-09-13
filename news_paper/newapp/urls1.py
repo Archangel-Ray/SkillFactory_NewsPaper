b99344ba78867690282acb856c1ccc0132085f69
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-13 15:33:28 +0300

    Модуль D18.6 - указал путь для представления для новостей

--- news_paper/newapp/urls1.py
+++ news_paper/newapp/urls1.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import NewsCreate, NewsUpdate, NewsDelete
+from .views import NewsCreate, NewsUpdate, NewsDelete, ListOfNews
 
 urlpatterns = [
+    path("", ListOfNews.as_view(), name="список всех новости"),
     path("create/", NewsCreate.as_view(), name="создание новости"),
     path("<int:pk>/edit/", NewsUpdate.as_view(), name="редактирование новости"),
     path("<int:pk>/delete/", NewsDelete.as_view(), name="удаление новости"),
