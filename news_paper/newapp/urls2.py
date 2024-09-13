42e8d7736d84c3e2d12aa7e39a9fa8fcfca104d2
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-13 15:41:18 +0300

    Модуль D18.6 - указал путь для представления для списка статей

--- news_paper/newapp/urls2.py
+++ news_paper/newapp/urls2.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import NewsCreate, NewsUpdate, NewsDelete
-
+from .views import NewsCreate, NewsUpdate, NewsDelete, ListOfArticles
 
 urlpatterns = [
+    path("", ListOfArticles.as_view(), name="список всех статей"),
     path('create/', NewsCreate.as_view(), name='создание статьи'),
     path('<int:pk>/edit/', NewsUpdate.as_view(), name='редактирование статьи'),
     path('<int:pk>/delete/', NewsDelete.as_view(), name='удаление статьи'),
