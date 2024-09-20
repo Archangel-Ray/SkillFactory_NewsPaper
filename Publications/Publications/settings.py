ca3a2cb6ce2696ca2f3de59bda1b40ee69f622c5
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-20 12:54:33 +0300

    добавил настройку отправки почты через Яндекс
    указал эл.адрес по умолчанию. его же прописал серверу и админу.
    __________________________________________________________________
    была какая-то возможность сохранять конфиденциальные данные прямо
    в окружении, но я тогда не разобрался с этим вопросом, а сейчас
    нет времени на это. обязательно надо будет разобраться позже.
    сейчас, по старинке, положил всё отдельно от проекта.
    __________________________________________________________________
    вот когда голова свежая всё решается значительно проще.
    ошибка была, потому что надо всё доделывать.

--- Publications/Publications/settings.py
+++ Publications/Publications/settings.py
@@ -145,3 +145,19 @@ ACCOUNT_UNIQUE_EMAIL = True  # поле почты уникально
 ACCOUNT_USERNAME_REQUIRED = False  # выключает обязательное заполнение имени
 ACCOUNT_AUTHENTICATION_METHOD = 'email'  # метод аутентификации через поле почты
 ACCOUNT_EMAIL_VERIFICATION = 'none'  # проверка почты (не знаю как подключить)
+
+# настройка отправки писем по эл.почте
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = open(
+    "G:/Python_projects/all_secret_codes_are_here/Yandex email/login.txt"
+).read()
+EMAIL_HOST_PASSWORD = open(
+    "G:/Python_projects/all_secret_codes_are_here/Yandex email/password.txt"
+).read()
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = open(
+    "G:/Python_projects/all_secret_codes_are_here/Yandex email/email.txt"
+).read()
+SERVER_EMAIL = DEFAULT_FROM_EMAIL  # адрес сервера
+ADMINS = (("admin", DEFAULT_FROM_EMAIL),)  # список админов
