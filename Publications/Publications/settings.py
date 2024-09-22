71cc1138b6c5dced27a6a7b27a334a583660e795
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-22 12:47:06 +0300

    СК-редактор на ГитХаб с инструкцией установки
    https://github.com/django-ckeditor/django-ckeditor
    
    включил отладчик, вернул ссылку на файлы статики и дописал ссылку
    на директорию (на всякий случай). И выключил РУТ.
    __________________________________________________________________
    когда коммитил прошлый раз появилась ошибка "Commit contains
    problems". впервые её вижу. говорит что, что-то с файлами
    редактора не ладно. не понимаю, что ему не понравилось.

--- Publications/Publications/settings.py
+++ Publications/Publications/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-yuc9^%o9kwtvz!#yfij9s#8a)o5@l(9+7$nevoq61uj)tpv5*!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -125,8 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-# STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
