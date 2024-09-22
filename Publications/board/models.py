6335a901b854a9ca85ca1b09c8cb700301c8d0e4
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-22 13:22:56 +0300

    прописал модель объявления

--- Publications/board/models.py
+++ Publications/board/models.py
@@ -1,4 +1,7 @@
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -6,3 +9,18 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Publication(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = RichTextUploadingField(verbose_name="Содержание")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('publication_detail', args=[str(self.id)])
