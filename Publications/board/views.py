db01e6286968252c0c47a5f544b2a27c4e367a3f
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-23 14:21:48 +0300

    сделал представление для всех публикаций

--- Publications/board/views.py
+++ Publications/board/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Publication
 
 
 def main(request):
     return render(request, "basic.html")
+
+
+class PublicationsList(ListView):
+    model = Publication
+    ordering = '-update_time'
+    template_name = 'publications.html'
+    context_object_name = 'publications'
