3fd12635ef2a2f2214da395633fad80af74aa653
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-24 13:50:47 +0300

    ссылка на отдельную публикацию

--- Publications/board/urls.py
+++ Publications/board/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import PublicationsList
+from .views import PublicationsList, PublicationDetail
 
 urlpatterns = [
     path("", PublicationsList.as_view(), name='основная страница'),
+    path("<int:id>/", PublicationDetail.as_view(), name='публикация'),
 ]
