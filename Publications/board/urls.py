ed0c78a5d89edd92cc298854eda2d28d73a21354
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-24 14:42:16 +0300

    ссылка на создание новой публикации

--- Publications/board/urls.py
+++ Publications/board/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PublicationsList, PublicationDetail
+from .views import PublicationsList, PublicationDetail, PublicationCreate
 
 urlpatterns = [
     path("", PublicationsList.as_view(), name='основная страница'),
     path("<int:id>/", PublicationDetail.as_view(), name='публикация'),
+    path("create/", PublicationCreate.as_view(), name='создать публикацию'),
 ]
