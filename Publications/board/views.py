6522e48eeab899b67be14c3e2a31e496abe8f6bf
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-24 13:50:02 +0300

    представление для отдельной публикации

--- Publications/board/views.py
+++ Publications/board/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Publication
 
@@ -13,3 +13,10 @@ class PublicationsList(ListView):
     ordering = '-update_time'
     template_name = 'publications.html'
     context_object_name = 'publications'
+
+
+class PublicationDetail(DetailView):
+    model = Publication
+    template_name = 'publication.html'
+    context_object_name = 'publication'
+    pk_url_kwarg = 'id'
