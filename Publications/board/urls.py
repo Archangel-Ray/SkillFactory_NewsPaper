50d9b10bc91e6c0a95684107609c2203f3d82b59
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-25 14:44:17 +0300

    ссылка на редактирование публикации

--- Publications/board/urls.py
+++ Publications/board/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import PublicationsList, PublicationDetail, PublicationCreate
+from .views import PublicationsList, PublicationDetail, PublicationCreate, PublicationEdit
 
 urlpatterns = [
     path("", PublicationsList.as_view(), name='основная страница'),
     path("<int:id>/", PublicationDetail.as_view(), name='publication_detail'),
     path("create/", PublicationCreate.as_view(), name='создать публикацию'),
+    path("<int:id>/edit/", PublicationEdit.as_view(), name='изменить публикацию'),
 ]
