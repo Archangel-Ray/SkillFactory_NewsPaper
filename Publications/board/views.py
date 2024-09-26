e2cfcf75139b07edf82f6798cb952a84f2e740cc
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-27 01:15:44 +0300

    добавил форму комментария в представление отдельной публикации
    на это ушёл практически день. как сделать форму на отдельной
    странице я уже понял. а вот как добавить её в уже существующую
    страницу оказалось не так-то просто. по крайней мере этого нам не
    рассказывали. в конце концов наткнулся на этот блог и сделал как
    там указано. да, я не всё понял, что там делается и не с первого
    раза всё заработало. там есть ещё один пример, но намного сложней
    ну как мне показалось. возможно позже разберусь и с ним.

--- Publications/board/views.py
+++ Publications/board/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic.edit import FormMixin
 
-from .forms import PublicationForm
+from .forms import PublicationForm, CommentForm
 from .models import Publication
 
 
@@ -17,12 +19,39 @@ class PublicationsList(ListView):
     context_object_name = 'publications'
 
 
-class PublicationDetail(DetailView):
+class PublicationDetail(FormMixin, DetailView):
     model = Publication
     template_name = 'publication.html'
     context_object_name = 'publication'
-    pk_url_kwarg = 'id'
     pk_url_kwarg = 'pk'
+    form_class = CommentForm
+    """
+    добавил форму для добавления комментария так как показано здесь:
+    https://www.thecoderscamp.com/django-implementing-a-form-within-a-generic-detailviewdjango/
+    """
+
+    def get_success_url(self):
+        return reverse('отдельная публикация', kwargs={'pk': self.object.id})
+
+    def get_context_data(self, **kwargs):
+        context = super(PublicationDetail, self).get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.publication = self.object
+        comment.user = self.request.user
+        form.save()
+        return super(PublicationDetail, self).form_valid(form)
 
 
 class PublicationCreate(LoginRequiredMixin, CreateView):
