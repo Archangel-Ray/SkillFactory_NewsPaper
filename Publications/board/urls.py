b622faeadef6292c3f52d63012b9067cd609e76e
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-27 13:31:33 +0300

    сделал роут на представление отображения откликов

--- Publications/board/urls.py
+++ Publications/board/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import PublicationsList, PublicationDetail, PublicationCreate, PublicationEdit
+from .views import PublicationsList, PublicationDetail, PublicationCreate, PublicationEdit, ResponsesToMyPublications
 
 urlpatterns = [
     path("", PublicationsList.as_view(), name='основная страница'),
     path("<int:pk>/", PublicationDetail.as_view(), name='отдельная публикация'),
     path("create/", PublicationCreate.as_view(), name='создать публикацию'),
     path("<int:id>/edit/", PublicationEdit.as_view(), name='изменить публикацию'),
+    path('private/', ResponsesToMyPublications.as_view(), name='личный кабинет'),
 ]
