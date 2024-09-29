c5655d1ed2367440c51c7474cd07520a832113c9
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-29 10:40:28 +0300

    отфильтровал публикации которые отправляются в фильтр
    пользователя, я получил. а как сказать фильтру какие публикации
    нужны?... если б я понимал как это работает было бы легче.
    в конечном итоге фильтр теперь в курсе какие из публикаций нужны.
    осталось сообразить как отфильтровать мои отклики.

--- Publications/board/filters.py
+++ Publications/board/filters.py
@@ -1,9 +1,13 @@
 from django_filters import FilterSet
 
-from .models import Comment
+from .models import Comment, Publication
 
 
 class FilterByPublications(FilterSet):
     class Meta:
         model = Comment
         fields = {'publication'}
+
+    def __init__(self, *args, **kwargs):
+        super(FilterByPublications, self).__init__(*args, **kwargs)
+        self.filters['publication'].queryset = Publication.objects.filter(user_id=kwargs['request'])
