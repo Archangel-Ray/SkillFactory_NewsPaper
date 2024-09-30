833f7e27156021048897fe2b3a4f4fc3be149472
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2024-09-30 10:57:29 +0300

    ссылка на удаление отклика

--- Publications/board/urls.py
+++ Publications/board/urls.py
@@ -7,6 +7,7 @@ from .views import (
     PublicationEdit,
     ResponsesToMyPublications,
     accept_the_response,
+    delete_response,
 )
 
 urlpatterns = [
@@ -15,5 +16,6 @@ urlpatterns = [
     path("create/", PublicationCreate.as_view(), name='создать публикацию'),
     path("<int:id>/edit/", PublicationEdit.as_view(), name='изменить публикацию'),
     path('private/', ResponsesToMyPublications.as_view(), name='посмотреть отклики'),
-    path('<int:pk>/accept/', accept_the_response, name='принять отклик')
+    path('<int:pk>/accept/', accept_the_response, name='принять отклик'),
+    path('<int:pk>/delete/', delete_response, name='удалить отклик'),
 ]
